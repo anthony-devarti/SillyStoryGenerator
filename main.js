65de8de1d99734d1aca9de020abae612f710079c
Anthony DeVarti <anthony.devarti@gmail.com>
2022-02-08 00:15:52 +0000

    test for accuracy

--- main.js
+++ main.js
@@ -42,15 +42,18 @@ function result() {
 
   if(customName.value !== '') {
     const name = customName.value;
-
+    newStory = newStory 
+      .replace ('Bob', name);
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
-
+    const weight = Math.round(300*0.0714286) + ' stone';
+    const temperature =  Math.round((94-32)*5/9) + ' centigrade';
+    newStory = newStory
+      .replace ('300 pounds', weight)
+      .replace ('94 fahrenheit', temperature);
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
